e7bebb47ec5069ebf15091b9ef1d4ee3fec7555d
mercedesaven <mercedes.aven@gmail.com>
2019-05-21 20:54:38 -0300

    con mensaje y botoncito de color

--- todo.js
+++ todo.js
@@ -1,97 +1,114 @@
 var task, taskLi, newTask, taskPending, taskDone, btnInput
 
-var taskInput = [ 
-   {text: 'uno', isPending: true}, 
-   {text: 'dos', isPending: true}, 
-   {text: 'tres', isPending: true},
+var taskInput = [
+    // {text: 'uno', isPending: true}, 
+    //{text: 'dos', isPending: true}, 
+    //{text: 'tres', isPending: true},
 ]
 console.log(taskInput)
 
-var handleKeyPress = function(event){
-        
-    if (event.code === 'Enter'){
-      sendTask(event)
-    }
-  }
+var handleKeyPress = function (event) {
 
-var deleteItem = function(btn){
+    if (event.code === 'Enter') {
+        sendTask(event)
+    }
+}
+
+var deleteItem = function (btn) {
     taskInput.splice(btn.id, 1)
     printTaskList()
 }
 
-var toggleItem = function(btn){
-    taskInput[btn.id].isPending = !taskInput[btn.id].isPending 
+var toggleItem = function (btn) {
+    taskInput[btn.id].isPending = !taskInput[btn.id].isPending
     printTaskList()
-} 
+}
 
 
 
 
-var printTaskList = function(){
-    taskPending = document.getElementById ('pending')
+var printTaskList = function () {
+    taskPending = document.getElementById('pending')
     taskPending.innerHTML = ''
-    taskPending.classList.add('ul-pending')
-   
+
 
     taskDone = document.getElementById('done')
-    taskDone.innerHTML =''
-    taskDone.classList.add('ul-done')
+    taskDone.innerHTML = ''
     
-   
-    taskInput.map(function(text, index){
+
+
+    taskInput.map(function (text, index) {
         taskLi = document.createElement('li')
-        
+
         taskPending.appendChild(taskLi)
-        taskPending.classList.remove("ul-pending")  // solo cuando todo esta deleteado
+        //taskPending.classList.remove("ul-pending")  // solo cuando todo esta deleteado
         taskLi.innerText = text.text
         taskDone.appendChild(taskLi)
-        taskDone.classList.remove("ul-done") // solo cuando todo esta deleteado
-        
+        // taskDone.classList.remove("ul-done") // solo cuando todo esta deleteado
+
         var itemBtnDelete = document.createElement('button')
         itemBtnDelete.classList.add('deleteBtn')
-   
-        itemBtnDelete.id = index 
-        itemBtnDelete.onclick = function() {deleteItem(this)} 
+
+        itemBtnDelete.id = index
+        itemBtnDelete.onclick = function () { deleteItem(this) }
 
 
 
         var itemBtn = document.createElement('button')
         itemBtn.classList.add('checkBtn')
-     
-        itemBtn.id = index 
-        itemBtn.onclick = function() {toggleItem(this)} 
-    
-    
-   
-         
- 
-    taskLi.appendChild(itemBtnDelete)
-    taskLi.appendChild(itemBtn)
+        
 
-    if (text.isPending){
-        taskPending.appendChild(taskLi)
-        }else {
+        itemBtn.id = index
+        itemBtn.onclick = function () { toggleItem(this) }
+
+
+
+
+
+
+        taskLi.appendChild(itemBtnDelete)
+        taskLi.appendChild(itemBtn)
+
+        if (text.isPending) {
+            taskPending.appendChild(taskLi)
+        } else {
             taskDone.appendChild(taskLi)
+            itemBtn.classList.toggle('checkBtnColor')
         }
-  
-     
-    
+
+
+
     })
+
+    if(taskPending.childElementCount === 0){
+        taskPending.classList.add('ul-pending')
+    } else {
+        taskPending.classList.remove('ul-pending')
     }
 
+    if(taskDone.childElementCount === 0){
+        taskDone.classList.add('ul-done')
+    } else {
+        taskDone.classList.remove('ul-done')
+    }
 
-var sendTask = function(){
+    if( taskInput !== taskPending){
+
+        
+    }
+}
+
+
+var sendTask = function () {
     var inputBox = document.getElementById('commentBox')
-    var newInput= inputBox.value 
-    if (newInput !== ''){
-        inputBox.value=''
+    var newInput = inputBox.value
+    if (newInput !== '') {
+        inputBox.value = ''
         taskInput.unshift({
             text: newInput,
             isPending: true
         })
-    printTaskList()
+        printTaskList()
     }
 
 } 
-    
-
